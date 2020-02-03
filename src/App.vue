f5a36829c48decfa303ac1092b1ab224f29359b6
boskobezik <buddhacatmonk@gmail.com>
2020-02-03 13:13:56 +0100

    fixed icons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" class="page-container">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+        <router-view />
   </div>
 </template>
 
 <style>
+.md-app {
+    max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
