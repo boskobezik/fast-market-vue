1666df2e33a682a39ef7eb8af4b4d337ac3e4533
boskobezik <buddhacatmonk@gmail.com>
2020-02-03 13:35:21 +0100

    getting started

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,52 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app" class="page-container">
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+        <md-app>
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">My Title</span>
+      </md-app-toolbar>
+
+      <md-app-drawer md-permanent="full">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          Navigation
+        </md-toolbar>
+
+        <md-list>
+          <md-list-item to="/addProduct">
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Sent Mail</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>delete</md-icon>
+            <span class="md-list-item-text">Trash</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Spam</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <router-view />
+       
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
 <style>
 .md-app {
-  max-height: 400px;
-  border: 1px solid rgba(#000, 0.12);
-}
+    max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
