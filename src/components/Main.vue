4557f7dacc32ea318c7511d2f644029acc595a76
sahzudin <s.mahmic@live.com>
2020-02-03 13:37:07 +0100

    ttt

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -95,13 +95,12 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .md-app {
   max-height: 400px;
   border: 1px solid rgba(#000, 0.12);
 }
 
-// Demo purposes only
 .md-drawer {
   width: 230px;
   max-width: calc(100vw - 125px);
