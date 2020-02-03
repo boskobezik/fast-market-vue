6810a1cf724eb4a6f29236ce27d6f4b23ed384d2
sahzudin <s.mahmic@live.com>
2020-02-03 12:34:58 +0100

    innitial setup

---
+++ src/components/AddProduct.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Dodaj proizvod page</div>
+</template>
+
+<script>
+export default {
+  name: "AddProduct"
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import AddProduct from "../components/AddProduct.vue";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+
+Vue.use(VueMaterial);
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
+const routes = [{
     path: "/",
     name: "home",
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/addproduct",
+    name: "addproduct",
+    component: AddProduct
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: "history"
 });
 
 export default router;
