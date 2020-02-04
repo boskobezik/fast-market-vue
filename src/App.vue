902d914d55fc1cbb8490a35222d9e25d8842a42e
boskobezik <buddhacatmonk@gmail.com>
2020-02-04 14:36:35 +0100

    idk2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div v-if="isLogged">
+    <div v-if="!isLogged">
       <b-navbar toggleable="lg" type="dark" variant="info">
         <b-navbar-brand href="#">FastStore</b-navbar-brand>
 
@@ -8,44 +8,13 @@
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-              ></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                >Search</b-button
-              >
-            </b-nav-form>
-
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template v-slot:button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
+            <b-nav-item to="addproduct">Add Product</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
-      <router-view />
+      <router-view class="router-app" />
     </div>
-    <router-view v-if="!isLogged" />
+    <router-view v-if="isLogged" />
   </div>
 </template>
 
@@ -61,36 +30,18 @@ export default {
     }
   },
   mounted: function() {
-    if (!this.isLogged) this.$router.replace("/login");
+    // if (!this.isLogged) this.$router.replace("/login");
   }
 };
 </script>
 
 <style>
-.md-app{
-  border: 1px solid rgba(#000, 0.12);
+.router-app {
+  padding: 25px 25%;
 }
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  text-align: center;
   color: #2c3e50;
-}
-
-.content {
-  min-height: 100% !important;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
